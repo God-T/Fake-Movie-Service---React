bdeb0c383f406244960b6eec6abf522f0125fb52
Shuyu <s.zong@student.unsw.edu.au>
2020-06-18 19:03:06 +1000

    handle page change

--- src/components/common/pagination.jsx
+++ src/components/common/pagination.jsx
@@ -7,6 +7,9 @@ const Pagination = ({ moviesCount, pageSize, onPageChange, currPage }) => {
   return (
     <nav>
       <ul className="pagination">
+        <li className={currPage === 1 ? "page-item disabled" : "page-item"}>
+          <a className="page-link">Prev</a>
+        </li>
         {pages.map((page) => (
           <li
             key={page}
@@ -17,6 +20,13 @@ const Pagination = ({ moviesCount, pageSize, onPageChange, currPage }) => {
             </a>
           </li>
         ))}
+        <li
+          className={
+            currPage === pagesCount ? "page-item disabled" : "page-item"
+          }
+        >
+          <a className="page-link">Next</a>
+        </li>
       </ul>
     </nav>
   );
