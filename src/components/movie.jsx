9a398214ed238a885b4c2310e28e1bdf357e434a
Shuyu <s.zong@student.unsw.edu.au>
2020-06-18 14:59:27 +1000

    display movies on table

---
+++ src/components/movie.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Movie extends Component {
+
+    render() {
+        const {movie, onDelete} = this.props
+        return ( 
+
+
+        );
+    }
+}
+ 
+export default Movie;
