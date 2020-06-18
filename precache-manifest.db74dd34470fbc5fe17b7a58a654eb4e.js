3fc7f205829588fc33375372b9349aa06ac7bc4d
Shuyu <s.zong@student.unsw.edu.au>
2020-06-18 14:06:08 +1000

    Updates

---
+++ precache-manifest.db74dd34470fbc5fe17b7a58a654eb4e.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f38f3db3fef64d6173b1152412cd528e",
+    "url": "/fake-movie-service/index.html"
+  },
+  {
+    "revision": "b9e5476fe4bc006c7bb9",
+    "url": "/fake-movie-service/static/css/2.e009fad0.chunk.css"
+  },
+  {
+    "revision": "1884c6e74def0c27a45c",
+    "url": "/fake-movie-service/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "b9e5476fe4bc006c7bb9",
+    "url": "/fake-movie-service/static/js/2.e3a1c7d7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/fake-movie-service/static/js/2.e3a1c7d7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1884c6e74def0c27a45c",
+    "url": "/fake-movie-service/static/js/main.3f16536f.chunk.js"
+  },
+  {
+    "revision": "ca9a27ce22454db99d11",
+    "url": "/fake-movie-service/static/js/runtime-main.daece00c.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/fake-movie-service/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/fake-movie-service/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/fake-movie-service/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/fake-movie-service/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/fake-movie-service/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/fake-movie-service/static/media/logo.5d5d9eef.svg"
+  }
+]);
